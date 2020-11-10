25ea1b610ce5bb3eefa7caa257c7f6ef5d8c8056
BadRAM <dgamebillda@gmail.com>
2020-11-10 14:27:04 -0800

    Added settings menu

---
+++ Assets/Scripts/UX/Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Canvas topMenu;
+    [SerializeField] private Canvas settingsMenu;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        topMenu.enabled = true;
+        settingsMenu.enabled = false;
+    }
+
+    public void OpenSettings()
+    {
+        topMenu.enabled = false;
+        settingsMenu.enabled = true;
+        FindObjectOfType<SettingsMenu>().ResetFields();
+    }
+
+    public void ReturnToTop()
+    {
+        topMenu.enabled = true;
+        settingsMenu.enabled = false;
+    }
+}
