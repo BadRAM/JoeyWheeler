2698705006d0316b64db07430828dd6b7bba3525
Siming <yangsiming1999@gmail.com>
2020-12-08 19:18:27 -0800

    added 3x3 and 5x5 grids, also fixed issue with tiles not alligning correctly?

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Generates a level
+/// </summary>
+
 public class LevelGeneration : MonoBehaviour
 {
     [SerializeField] private GameObject levelBlock;
@@ -9,9 +14,8 @@ public class LevelGeneration : MonoBehaviour
     [SerializeField] private int levelAmount;
     private List<GameObject> gObjects = new List<GameObject>();
     private GameObject[] exitList;
-    [SerializeField] private List<Vector3> UsedSpaces = new List<Vector3>();
+    private List<Vector3> UsedSpaces = new List<Vector3>();
 
-    private Transform test;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +27,19 @@ public class LevelGeneration : MonoBehaviour
     {
     }
 
+    // Function used to generate Level
     void GenerateLevel()
     {
-
-        for(var i = 0; i < levelAmount; i++)
+        exitList = GameObject.FindGameObjectsWithTag("ExitPoint");
+        for (var i = 0; i < levelAmount; i++)
         {
             GameObject obj;
             bool valid = true;
             List<Vector3> newUsedSpaces = new List<Vector3>();
+            // First block is always 1x1
             if (i == 0)
             {
-                obj = Instantiate(RandomTile(), new Vector3(0, 0, 0), Quaternion.identity);
+                obj = Instantiate(levelBlock, new Vector3(0, 0, 0), Quaternion.identity);
                 obj.transform.parent = transform;
                 obj.name = $"Flat {i}";
                 UsedSpaces.Add(new Vector3(0, 0, 0));
@@ -41,14 +47,14 @@ public class LevelGeneration : MonoBehaviour
             }
             else
             {
+                // Gets a random block and places blocks next to each other
                 Transform ExitPoint = getRandomTarget();
                 obj = Instantiate(RandomTile(), ExitPoint.position, Quaternion.identity);
-                obj.transform.parent = transform;
                 obj.name = $"Flat {gObjects.Count}";
                 obj.transform.parent = transform;
                 for (var j = 0; j < obj.transform.childCount; j++)
                 {
-                    if(j == 0)
+                    if (j == 0)
                     {
                         moveObjectNextTo(ExitPoint.transform, obj.transform.GetChild(j));
                         newUsedSpaces.Add(Vector3Int.RoundToInt(ExitPoint.position));
@@ -58,24 +64,24 @@ public class LevelGeneration : MonoBehaviour
                         Transform extendPoint = getRandomExit(obj.transform.GetChild(j - 1), "ExtendPoint");
                         Transform extendRec = getRandomExit(obj.transform.GetChild(j), "ExtendRec");
                         valid = extendObjectNextTo(extendPoint, extendRec);
-                        if(valid == false)
+                        if (valid == false)
                         {
-                            Destroy(obj);
+                            DestroyImmediate(obj);
                             break;
                         }
                         newUsedSpaces.Add(Vector3Int.RoundToInt(extendPoint.position));
                     }
-                    if (valid)
-                    {
-                        UsedSpaces.AddRange(newUsedSpaces);
-                    }
-                    else
-                    {
-                        obj = Instantiate(levelBlock, ExitPoint.position, Quaternion.identity);
-                        obj.transform.parent = transform;
-                        moveObjectNextTo(ExitPoint.transform, obj.transform.GetChild(0));
-                        UsedSpaces.Add(Vector3Int.RoundToInt(ExitPoint.position));
-                    }
+                }
+                if (valid)
+                {
+                    UsedSpaces.AddRange(newUsedSpaces);
+                }
+                else
+                {
+                    obj = Instantiate(levelBlock, ExitPoint.position, Quaternion.identity);
+                    obj.transform.parent = transform;
+                    moveObjectNextTo(ExitPoint.transform, obj.transform.GetChild(0));
+                    UsedSpaces.Add(Vector3Int.RoundToInt(ExitPoint.position));
                 }
                 gObjects.Add(obj);
 
@@ -84,34 +90,20 @@ public class LevelGeneration : MonoBehaviour
         }
     }
 
-    private Transform getRandomTarget()
-    {
-        Transform toReturn;
-        while (true)
-        {
-            toReturn = exitList[Random.Range(0, exitList.Length)].transform;
-            toReturn.position = Vector3Int.RoundToInt(toReturn.position);
-            Debug.Log(toReturn.position);
-            if (UsedSpaces.Contains(Vector3Int.RoundToInt(toReturn.position)) == false)
-            {
-                return toReturn;
-            }
-        }
-    }
-
+    // Moves Object next to selected exitpoint
     private void moveObjectNextTo(Transform ExitPoint, Transform obj)
     {
-        Debug.Log(ExitPoint);
+        Debug.Log(ExitPoint.parent.parent.name);
         obj.position = ExitPoint.position;
         Transform newExit = getRandomExit(obj);
         RotateObject(ExitPoint, newExit);
     }
 
+    // Places extension object next to each other using their extendpoint and extendRec
     private bool extendObjectNextTo(Transform ExtendPoint, Transform ExtendRec)
     {
         if (UsedSpaces.Contains(Vector3Int.RoundToInt(ExtendPoint.position)) == true)
         {
-            Debug.Log("not Valid");
             return false;
         }
         ExtendRec.parent.position = ExtendPoint.position;
@@ -119,14 +111,12 @@ public class LevelGeneration : MonoBehaviour
         return true;
     }
     
+    // Rotates object until newexitpoint is aligned with the parent position of exitPoint
     private void RotateObject(Transform exitPoint, Transform newExitPoint)
     {
-        //Debug.Log($"Exit point: {exitPoint} {exitPoint.parent}");
-        //Debug.Log($"New exit Point: {newExitPoint} {newExitPoint.parent}");
         for(int i = 0; i < 4; i++)
         {
             newExitPoint.parent.Rotate(0, 90, 0);
-            //Debug.Log(newExitPoint.position);
             if (exitPoint.parent.position == newExitPoint.position)
             {
                 newExitPoint.gameObject.SetActive(false);
@@ -136,27 +126,46 @@ public class LevelGeneration : MonoBehaviour
         }
     }
 
+    // Returns a random tile from the tilearray 
     private GameObject RandomTile()
     {
         return tileArray[Random.Range(0, tileArray.Length)];
     }
 
+    /// <summary>
+    /// Gets a random object with the same same tag in the paremeter
+    /// </summary>
+    /// <param name="Target"> The Target Object</param>
+    /// <param name="tagString">The name of the tag</param>
+    /// <returns>Random Transform with tagString as tag</returns>
     private Transform getRandomExit(Transform Target, string tagString = "ExitPoint")
     {
-        Debug.Log(Target);
         List<Transform> listOfNewExits = new List<Transform>();
         foreach(Transform child in Target)
         {
-            //Debug.Log(child);
             if (child.tag == tagString)
             {
-                //Debug.Log(child.tag);
                 listOfNewExits.Add(child);
             }
         }
-        //Debug.Log(listOfNewExits.Count);
         return listOfNewExits[Random.Range(0, listOfNewExits.Count)];
-        //return listOfNewExits[5];
+    }
+    /// <summary>
+    /// Gets a random exit target from the exitList, retries if target is invalid
+    /// </summary>
+    /// <returns>Transform of exit</returns>
+    private Transform getRandomTarget()
+    {
+        Transform toReturn;
+        while (true)
+        {
+            toReturn = exitList[Random.Range(0, exitList.Length)].transform;
+            toReturn.position = Vector3Int.RoundToInt(toReturn.position);
+            if (UsedSpaces.Contains(Vector3Int.RoundToInt(toReturn.position)) == false)
+            {
+                return toReturn;
+            }
+        }
     }
 
 }
